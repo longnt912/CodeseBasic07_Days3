ddbcee1cd72de6fb69bc9bb4a69ea414aac7f355
longnt912 <longchivasvolka@gmail.com>
2021-05-16 16:55:13 +0700

    Chua lam duoc

--- src/Hoten.java
+++ src/Hoten.java
@@ -2,15 +2,4 @@ import java.util.Scanner;
 
 public class Hoten {
         public static void main(String[] args) {
-            double n;
-            Scanner obj = new Scanner(System.in);
-            do {
-                System.out.print("Nhập vào một số: ");
-                n = obj.nextDouble();
-            } while (n < 0 || n > 100);
-        }
 
-        public static void readNumber() {
-
-        }
-    }
